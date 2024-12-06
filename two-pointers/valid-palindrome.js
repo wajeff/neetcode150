66d97e77fedecb6debf96d20f82bfaa872438b79
WaeBae <jrwuoft@gmail.com>
2024-12-05 20:10:48 -0500

    Two Pointers valid-palindrome.js solved

---
+++ two-pointers/valid-palindrome.js
@@ -0,0 +1,32 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  isPalindrome(s) {
+    const alphabetExp = new RegExp(/[^a-z0-9]/gi)
+    s=s.replaceAll(alphabetExp,'').toLowerCase()
+
+    let left = 0 
+    let right = s.length-1;
+
+    if(s.length==2){
+      if(s[0]===s[1]){
+        return true;
+      }
+      else{
+        return false;
+      };
+    };
+
+    while(left<=right){
+
+      if(!(s[left]===s[right])){
+        return false;
+      };
+      left++
+      right--
+    };
+    return true;
+  };
+};
