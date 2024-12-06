128b9ff181ea16e1bd51e6c910858fc80b1bb140
WaeBae <jrwuoft@gmail.com>
2024-12-06 03:14:05 -0500

    Two pointers buy-and-sell stock completed

---
+++ two-pointers/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,23 @@
+class Solution {
+  /**
+   * @param {number[]} prices
+   * @return {number}
+   */
+  maxProfit (prices){
+      let left = 0;
+      let right = 1;
+      let max = 0;
+
+      while(right<prices.length){
+          if(prices[left]<prices[right]){
+              let profit = prices[right]-prices[left]
+              max = Math.max(profit,max)
+      }
+      else{
+          left = right
+      };
+      right++
+  }
+  return(max);
+};
+}
